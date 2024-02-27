ca8d318141704e5feaa22d15277726f493dca00a
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-02-27 11:42:00 -0600

    Clean up comments in settings files

--- city_scrapers/settings/base.py
+++ city_scrapers/settings/base.py
@@ -9,15 +9,6 @@ env_file = find_dotenv(".env.development")
 if os.path.exists(env_file):
     load_dotenv(env_file)
 
-# Scrapy settings for city_scrapers project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = "city_scrapers"
 
 SPIDER_MODULES = ["city_scrapers.spiders"]
